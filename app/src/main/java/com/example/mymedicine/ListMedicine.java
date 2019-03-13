31666cb8e5086df44e4843b1f1cfa071ad1193f3
DiptiHossain <48102436+DiptiHossain@users.noreply.github.com>
2019-03-13 18:31:34 +0000

    Update ListMedicine.java

--- app/src/main/java/com/example/mymedicine/ListMedicine.java
+++ app/src/main/java/com/example/mymedicine/ListMedicine.java
@@ -3,72 +3,162 @@ package com.example.mymedicine;
 import android.app.LoaderManager;
 import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
-import android.app.LoaderManager.LoaderCallbacks;
-import android.app.ProgressDialog;
-import android.content.CursorLoader;
-import android.content.Intent;
-import android.content.Loader;
-import android.content.SharedPreferences;
-import android.database.Cursor;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.provider.ContactsContract;
-import android.support.v7.app.AppCompatActivity;
-import android.view.KeyEvent;
-import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.EditorInfo;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import android.app.Activity;
-import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
+import java.util.ArrayList;
+
+
+import android.widget.ArrayAdapter;
+import android.widget.AdapterView;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+import com.google.firebase.database.ChildEventListener;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ListMedicine extends AppCompatActivity {
+    private ArrayList<String> myItems = new ArrayList<String>();
+    private ArrayList<String> myItems2 = new ArrayList<String>();
+    ArrayAdapter<String> adapter;
+    EditText editText;
+    Button button;
+    ListView listView;
+
+    Button deleteButton;
+
+    private Boolean itemSelected = false;
+    private int selectedPosition = 0;
+
+    private FirebaseDatabase database = FirebaseDatabase.getInstance();
+    private DatabaseReference dbRef = database.getReference();
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_med_list);
 
-        populateListView();
-        registerClickCallback();
-    }
+        editText = (EditText) findViewById(R.id.add_medicine);
+        button = (Button) findViewById(R.id.button);
+        listView = (ListView) findViewById(R.id.listViewMain);
+        deleteButton = (Button) findViewById(R.id.button2);
 
-    private void populateListView() {
-        // Create list of items
-        String[] myItems = {"Hydrocodone-Acetaminophen", "Simvastatin", "Prinivil (Lisinopril)", "ynthroid-Levothyroxine", "Norvasc-Amlodipine"};
 
-        // Build Adapter
-        // TODO: CHANGE THE [[ to a less than, ]] to greater than.
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(
+
+
+        myItems = new ArrayList<String>();
+        myItems2 = new ArrayList<String>();
+        myItems.add("Hydrocodone-Acetaminophen");
+        myItems.add("Simvastatin");
+
+
+
+        adapter = new ArrayAdapter<String>(
                 this,     // Context for the activity.
-                 R.layout.the_file,  // Layout to use (create)
+                R.layout.the_file,  // Layout to use (create)
                 myItems);   // Items to be displayed
 
-        // Configure the list view.
         ListView list = (ListView) findViewById(R.id.listViewMain);
+
+        addChildEventListener();
+
         list.setAdapter(adapter);
+
+        //user click a medicication from the list
+        list.setOnItemClickListener(
+                new AdapterView.OnItemClickListener() {
+                    public void onItemClick(AdapterView<?> parent,
+                                            View view, int position, long id) {
+                        selectedPosition = position;
+                        itemSelected = true;
+
+                    }
+                });
+                addChildEventListener();
+
+
+
     }
 
-    private void registerClickCallback() {
-        ListView list = (ListView) findViewById(R.id.listViewMain);
-        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    // show list data
+    private void addChildEventListener() {
+        ChildEventListener childListener = new ChildEventListener() {
+
             @Override
-            // TODO: CHANGE THE [[ to a less than, ]] to greater than.
-            public void onItemClick(AdapterView<?> paret, View viewClicked, int position, long id) {
-                TextView textView = (TextView) viewClicked;
-                String message = "You clicked # " + position
-                        + ", which is string: " + textView.getText().toString();
-                Toast.makeText(ListMedicine.this, message, Toast.LENGTH_LONG).show();
+            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                int i=0;
+                while(dataSnapshot.child("MEDICATIONS").child(String.valueOf(i)).exists()) {
+                    adapter.add(dataSnapshot.child("MEDICATIONS").child(String.valueOf(i)).getValue().toString());
+                    i++;
+                }
+                myItems2.add(dataSnapshot.getKey());
             }
-        });
+
+            @Override
+            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+
+            }
+
+            @Override
+            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+            }
+
+            @Override
+            public void onChildRemoved(DataSnapshot dataSnapshot) {
+                String key = dataSnapshot.getKey();
+                int index = myItems2.indexOf(key);
+
+                if (index != -1) {
+                    myItems.remove(index);
+                    myItems2.remove(index);
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+            }
+        };
+        dbRef.addChildEventListener(childListener);
     }
+
+    // add medicine to database after user click the add button
+    public void addItem(View view) {
+
+
+        String medicine = editText.getText().toString();
+        String key = dbRef.push().getKey();
+
+        editText.setText("");
+        dbRef.child(key).setValue(medicine);
+
+        adapter.notifyDataSetChanged();
+    }
+
+
+    // delete medicine from database after selecting the medicine and clicking the delete button
+    public void deleteItem(View view) {
+        listView.setItemChecked(selectedPosition, false);
+        dbRef.child(myItems2.get(selectedPosition)).removeValue();
+    }
+
+
 }
