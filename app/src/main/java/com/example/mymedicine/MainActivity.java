f1db425b1fe836c8bdad9da49161eb4252ba3ff2
lorexsan <lorenzosani97@gmail.com>
2019-03-21 12:47:06 +0000

    layout fix

--- app/src/main/java/com/example/mymedicine/MainActivity.java
+++ app/src/main/java/com/example/mymedicine/MainActivity.java
@@ -52,4 +52,10 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        finishAffinity();
+    }
+
 }
